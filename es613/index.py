d0963647ba7e8fe2097bf4b772a87c2c69700217
Daniel Baumann <daniel1.baumann@student.uni-augsburg.de>
2020-05-23 22:59:05 +0200

    updated _SecondaryQuadtree

--- es613/index.py
+++ es613/index.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 """
+Author: Daniel Baumann
+Date: 23.05.2020
+eMail: baumann-dan@outlook.com
+
 This module contains classes for indexing data
+
+Classes:
+    SpatialIndex
+    PandasQuadtree
+    _SecondaryQuadtree1
+    Rectangle
+    QuadtreeDivison
+    
+Dependencies:
+    pandas
+    shapely
 
 Todo:
     
@@ -12,10 +27,9 @@ from abc import ABC, abstractmethod
 import pandas as pd
 import shapely.geometry as sg
 
-class SecondarySpatialIndex(ABC):
+class SpatialIndex(ABC):
     """
-    Abstract class defining methods implemented by secondary spatial index classes,
-    which provide an index referencing shapely geometry objects in a pandas.DataFrame object
+    Abstract class defining methods implemented by spatial index classes
     
     Attributes:
         df (pandas.DataFrame):
@@ -32,49 +46,107 @@ class SecondarySpatialIndex(ABC):
         pass
     
     
-class SecondaryQuadtree(SecondarySpatialIndex):
-    """
+class PandasQuadtree(SpatialIndex):
+    """This class provides a quadtree index for shapely.geometry objects stored in the column of a pandas.DataFrame object
+    
+    Attributes:
+        df (pandas.DataFrame): the dataframe for which the index will be greater
+        geometry_column (str): the name of the geometry column in @df
+        bbox (shapely.geometry.box): The extent for which the index will be created
+        capacity (int): the maximum number of geometries, a node in the index can reference. DEFAULT=8  
     
     """
-    def __init__(self, df:pd.DataFrame, geometry_column:object, capacity=8, bbox:sg.Polygon=None) -> None:
+    
+    def __init__(self, df:pd.DataFrame, geometry_column:object, bbox:sg.box=None, capacity=8):
+        """
+        Args:
+            df (pandas.DataFrame): the dataframe for which the index will be greater
+            geometry_column (str): the name of the geometry column in @df
+            bbox (shapely.geometry.box, optional): The extent for which the index will be created.
+                If no extent is passed. the extend will be calculated which will increase the instantiation time
+            capacity (int): the maximum number of geometries, a node in the index can reference. DEFAULT=8
+            
+        """
         self.df = df
         self.geometry_column = geometry_column
-        self.capacity = capacity
         self.bbox = bbox
-        self._qdt = _SecondaryQuadtree(bbox, capacity)
+        self.capacity = capacity
+        self._qdt = _SecondaryQuadtree1(Rectangle(*bbox.bounds), capacity)
         
         for i, row in self.df.iterrows():
-            print(i)
             self._qdt.insert(row[self.geometry_column], i)
             
     def insert(self, geometry:object, row_index:object):
+        """ This method inserts a geometry object into the tree
+        
+        Args:
+            geometry (shapely.geometry): The geometry object
+            row_index (object): The index of the corresponding row in the PandasQuadtree.df dataframe
+        """
         self._qdt.insert(geometry, row_index)
     
     def remove(self):
         pass
     
+    def range_query_candidates(self, geometry:object):
+        """
+        Args:
+            geometry (shapely.geometry): The query geometry
+            
+        Returns:
+            List[object]: returns List with ids of rows in the PandasQuadtree.df dataframe,
+                for which the topological predicate with the @geometry argument could be False.
+                The method only returns candidates. To only receive ids of rows for which the
+                topological predicate 'disjoint' is False, use the PandasQuadtree.range_query method.
+        """
+        return self._qdt.range_query(geometry)
+    
     def range_query(self, geometry:object):
-        
-        candidates = self._qdt.range_query(geometry)
+        """
+        Args:
+            geometry (shapely.geometry): The query geometry
+            
+        Returns:
+            List[object]: returns List with ids of rows in the PandasQuadtree.df dataframe,
+                for which the topological predicate with the @geometry argument is False.
+        """
+        candidates = self.range_query_candidates(geometry)
         return [pl for pl in candidates if not self.df.loc[pl][self.geometry_column].disjoint(geometry)]
         
         
-    
-class _SecondaryQuadtree(object):
+class _SecondaryQuadtree1(object):
     """
+    This class provides a quadtree index using the extent of the indexed geometries.
+    The items are stored in the nodes as well as the leaves of the tree.
+    This implies faster build and insert times with slower query times.
     """
-    def __init__(self, bbox:sg.Polygon, capacity) -> None:
-        self._bbox:sg.Polygon = bbox
+    def __init__(self, bbox:"Rectangle", capacity:int):
+        """
+        Args:
+            bbox (index.Rectangle): The extent for which the quadtree will be created
+            capacity (int): the maximum number of geometries, a node in the index can reference.
+        """
+        self._bbox:"Rectangle" = bbox
         self._capacity:int = capacity
-        self._geometries:sg.Polygon = []
+        self._geometries:object = []
         self._placeholders:object = []
         self._divided:boolean = False
         
     def insert(self, geom:object, placeholder:object) -> bool:
+        """ This method inserts an item into the tree. The geometry of the inserted item msut not
+        be disjoint with the _SecondaryQuadtree1._bbox.
         
-        geom = sg.box(*geom.bounds)
+        Args:
+            geom (shapely.geometry or index.Rectangle): The geomtery of the inserted item
+            placeholder (object): The placeholder of the item
+            
+        Returns:
+            bool: Returns True if item was inserted, else returns False
+        """
+        if not isinstance(geom, Rectangle):
+            geom = Rectangle(*geom.bounds)
         
-        if self._bbox.disjoint(geom) and not self._bbox.contains(geom):
+        if self._bbox.disjoint(geom):
             return False
         
         if len(self._geometries) < self._capacity:
@@ -89,66 +161,196 @@ class _SecondaryQuadtree(object):
         self._placeholders.append(placeholder)
             
     def subdivide(self) -> bool:
+        """This method turns the quadtree object into a node and creates four leafs for this node.
+        These leafes are named:
+            self._northwest
+            self._northeast
+            self._southeast
+            self._southwest
+            
+        Note:
+            If the extent of all geometries references by the _SecondaryQuadtree object, is equal,
+            no subdivision is performed
+            
+        Returns:
+            bool: Returns True if subdivision was performed, else returns False
+        """
+        rdiv:RectangleDivision = self._bbox.division()
         
-        qdiv:QuadtreeDivision = QuadtreeHelper.divide_quadtree(self._bbox)
-        
-        # check for equals
+        # check for equal geometries
         if len(self._geometries) > 1:
+            
             for i in range(len(self._geometries)):
+                
+                break_check = False
                 for x in range(i+1, len(self._geometries)):
                     if not (self._geometries[i] == self._geometries[x]):
+                        break_check=True
                         break
-                else:
-                    print("no-break")
-                    return False
+                if break_check:
+                    break
+                    
+            else:
+                return False
         
         self._divided = True
-        self._northwest = _SecondaryQuadtree(qdiv.northwest, self._capacity)
-        self._northeast = _SecondaryQuadtree(qdiv.northeast, self._capacity)
-        self._southeast = _SecondaryQuadtree(qdiv.southeast, self._capacity)
-        self._southwest = _SecondaryQuadtree(qdiv.southwest, self._capacity)
+        self._northwest = _SecondaryQuadtree1(rdiv.northwest, self._capacity)
+        self._northeast = _SecondaryQuadtree1(rdiv.northeast, self._capacity)
+        self._southeast = _SecondaryQuadtree1(rdiv.southeast, self._capacity)
+        self._southwest = _SecondaryQuadtree1(rdiv.southwest, self._capacity)
         self._boxlist = [self._northwest, self._northeast, self._southeast, self._southwest]
-        print("divided")
         return True
         
     def range_query(self, geom):
+        """This method returns the placeholder of all stored items, for which the extent is not disjoint
+            with the extent of @geom.
+            
+        Args:
+            geom (shapely.geometry or index.Rectangle): The query geometry
+            
+        Returns:
+            List[object]: returns List with placeholders of items in the index for which the topological predicate 'disjoint',
+                compared to the extent of @ggeom is False.
+        
+        """
+        if not isinstance(geom, Rectangle):
+            geom = Rectangle(*geom.bounds)
+        
+        candidates = [pl for pl, g in zip(self._placeholders, self._geometries) if not g.disjoint(geom)]
         
         if self._divided:
             pl_list = [box.range_query(geom) for box in self._boxlist if not geom.disjoint(box._bbox)]
+            pl_list.append(candidates)
             pl_list_flattend = [val for sublist in pl_list for val in sublist]
             pl_list_flattend_unique = list(set(pl_list_flattend))
             return pl_list_flattend_unique
         
         else:
-            return [pl for pl, g in zip(self._placeholders, self._geometries) if not g.disjoint(geom) or g.within(geom)]
+            return candidates
         
-class QuadtreeDivision(object):
+        
+class Rectangle(object):
+    """This class can be used to represent simple rectangles
     
-    def __init__(self, northwest, northeast, southeast, southwest):
-        self.northwest:sg.Polygon = northwest
-        self.northeast:sg.Polygon = northeast
-        self.southeast:sg.Polygon = southeast
-        self.southwest:sg.Polygon = southwest
+    Attributes:
+        xmin (float): minimum x-value
+        ymin (float): minimum y-value
+        xmax (float): maximum x-value
+        ymax (float): maximum y-value
+        width (float): the wifth of the rectangle
+        height (float): the height of the rectangle
+        bounds (tuple[float]): The bounds of the rectangle (xmin, ymin, xmax, ymax)
+    """
+    
+    def __init__(self, xmin, ymin, xmax, ymax):
+        """
+        Args:
+            xmin (float): minimum x-value
+            ymin (float): minimum y-value
+            xmax (float): maximum x-value
+            ymax (float): maximum y-value
+            
+        Raises:
+            ValueError: if xmin > xmax or ymin > ymax
+        """
+        if xmin > xmax or ymin > ymax:
+            raise ValueError()
+        
+        self.xmin = float(xmin)
+        self.ymin = float(ymin)
+        self.xmax = float(xmax)
+        self.ymax = float(ymax)
+        self.width = xmax - xmin
+        self.height = ymax - ymin
+        self.bounds = (self.xmin, self.ymin, self.xmax, self.ymax)
+        
+    def __eq__(self, other:"Rectangle") -> bool:
+        """
+        Args:
+            other (index.Rectangle): other Rectangle for which equality will be tested
+        
+        Returns:
+            bool: Returns true if self.xmin == other.xmin and self.ymin == other.ymin
+                and self.xmax == other.xmax and self.ymax == other.ymax
+                else returns False
+        """
+        if not isinstance(other, self.__class__):
+            return False
+        
+        elif self.xmin != other.xmin or self.ymin != other.ymin or self.xmax != other.xmax or self.ymax != other.ymax:
+            return False
+        
+        else:
+            return True
+    
+    def disjoint(self, other:"Rectangle") -> bool:
+        """
+        Args:
+            other (index.Rectangle): other rectangle for which it will be tested if @self is disjoint
+            
+        Returns:
+            bool: Returns True if, self.xmin > other.xmax or self.ymax < other.ymin or self.xmax < other.xmin or self.ymin > other.ymax
+                else returns False
+        """
+        return self.xmin > other.xmax or self.ymax < other.ymin or self.xmax < other.xmin or self.ymin > other.ymax
+    
+    def touches(self, other:"Rectangle") -> bool:
+        pass
+    
+    def overlaps(self, other:"Rectangle") -> bool:
+        pass
+    
+    def division(self) -> "RectangleDivision":
+        """
+        Returns:
+            index.RectangleDivison: Returns the divison of the rectangle into four equal parts
+        """
+        northwest = Rectangle(self.xmin, self.ymin + self.height/2, self.xmin + self.width/2, self.ymax)
+        northeast = Rectangle(self.xmin + self.width/2, self.ymin + self.height/2, self.xmax, self.ymax)
+        southeast = Rectangle(self.xmin + self.width/2, self.ymin, self.xmax, self.ymin + self.height/2)
+        southwest = Rectangle(self.xmin, self.ymin, self.xmin + self.width/2, self.ymin + self.height/2)
+        
+        return RectangleDivision(northwest, northeast, southeast, southwest)
+
+
+class RectangleDivision(object):
+    """ This class can be used to represent four eual parts of a rectangle
+    
+    Attributes:
+        northwest (index.Rectangle): the top-left part of the rectangle
+        northeast (index.Rectangle): the top-right part of the rectangle
+        southeast (index.Rectangle): the bottom-right part of the rectangle
+        southwest (index.Rectangle): the bottom-left part of the rectangle
+        boxlist (List[index.Rectangle]): List holding the parts of the rectangle [northwest, northeast, southeast, southwest]
+    """
+    
+    def __init__(self, northwest:"Rectangle", northeast:"Rectangle", southeast:"Rectangle", southwest:"Rectangle"):
+        """
+        Args:
+            northwest (index.Rectangle): the top-left part of the rectangle
+            northeast (index.Rectangle): the top-right part of the rectangle
+            southeast (index.Rectangle): the bottom-right part of the rectangle
+            southwest (index.Rectangle): the bottom-left part of the rectangle
+        """
+        self.northwest:Rectangle = northwest
+        self.northeast:Rectangle = northeast
+        self.southeast:Rectangle = southeast
+        self.southwest:Rectangle = southwest
         self.boxlist = [self.northwest, self.northeast, self.southeast, self.southwest]
         
-    def __getitem__(self, key):
+    def __getitem__(self, key) -> "Rectangle":
+        """
+        Returns:
+            index.Rectangle: 
+                RectangleDivison[0] == Rectangle.northwest
+                RectangleDivison[1] == Rectangle.northeast
+                RectangleDivison[2] == Rectangle.southeast
+                RectangleDivison[3] == Rectangle.southwest
+        """
         return self.boxlist[key]
         
         
-class QuadtreeHelper:
-    
-    @staticmethod
-    def divide_quadtree(quadtree_box:sg.Polygon) -> QuadtreeDivision:
-        
-        xmin, ymin, xmax, ymax = quadtree_box.bounds
-        width = xmax - xmin
-        height = ymax - ymin
-        northwest = sg.box(xmin, ymin + height/2., xmin + width/2., ymax)
-        northeast = sg.box(xmin + width/2., ymin + height/2, xmax, ymax)
-        southeast = sg.box(xmin + width/2., ymin, xmax, ymin + height/2.)
-        southwest = sg.box(xmin, ymin, xmin + width/2., ymin + height/2.)
-        
-        return QuadtreeDivision(northwest, northeast, southeast, southwest)
+#class QuadtreeHelper:
         
         
     
--- es613/tests/index_tests.py
+++ es613/tests/index_tests.py
@@ -1,69 +1,129 @@
 # -*- coding: utf-8 -*-
 """
+Author: Daniel Baumann
+Date: 23.05.2020
+eMail: baumann-dan@outlook.com
+
 This module contains the unittests for the es613.index module
+
 Todo:
 
 """
 import unittest
 import random
+import json
+from pathlib import Path
 
 import pandas as pd
 import shapely.geometry as sg
 
 from es613 import index
 
-class Test_SecondaryQuadtree(unittest.TestCase):
+class Test_PandasQuadtree(unittest.TestCase):
     
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         
+        # PandasQuadtree objects
+        cls.qdt1 = None
+        cls.qdt2 = None
+        
+        # test query polygons for qdt2
+        cls.query_polygon_df = None
+        
+        # defining directories and files
+        cls.current_directory = Path(__file__).parent.absolute()
+        cls.test_data_directory = cls.current_directory.parent.parent / "test_data"
+        cls.gemeinden_selection_features = cls.test_data_directory / "gemeinden_bayern_selection.geojson"
+        cls.gemeinden_query = cls.test_data_directory / "gemeinden_query.geojson"
+        
+        # defining PandasQuadtree1
         xmin, ymin, xmax, ymax = 0.0, 0.0, 10.0, 10.0
-        geometries = [sg.Point(random.uniform(xmin, xmax), random.uniform(ymin, ymax)) for _ in range(1000)]
-        ids = ["id{}".format(i) for i in range(1000)]
-        self.df1 = pd.DataFrame({"id":ids, "geometry":geometries})
-        self.df1.set_index("id", drop=False, inplace=True)
-        self.bbox1 = sg.box(xmin, ymin, xmax, ymax)
-        self.qdt1 = index.SecondaryQuadtree(self.df1, "geometry", capacity=4, bbox=self.bbox1)
+        geometries = [sg.Point(random.uniform(xmin, xmax), random.uniform(ymin, ymax)) for _ in range(100)]
+        ids = ["id{}".format(i) for i in range(100)]
+        cls.df1 = pd.DataFrame({"id":ids, "geometry":geometries})
+        cls.df1.set_index("id", drop=False, inplace=True)
+        cls.bbox1 = sg.box(xmin, ymin, xmax, ymax)
+        cls.qdt1 = index.PandasQuadtree(cls.df1, "geometry", capacity=10, bbox=cls.bbox1)
         
-        
-        
-    #===========================================================================
-    # def test_init(self):
-    #     for i in range(200):
-    #         self.qdt1.insert(sg.Point(1.1008764,1.10001), "id007")
-    #     #print(len(self.qdt1.range_query(self.bbox1)))
-    #===========================================================================
-        
-    def test_range_query(self):
+        # defining PandasQuadtree2
+        cls.gemeinden_df = pd.DataFrame(columns=["OBJID", "BEZ_GEM", "geometry"])
+        with open(cls.gemeinden_selection_features) as features_json:
+            gemeinden_dict = json.load(features_json)
+            cls.gemeinden_bbox = gemeinden_dict["bbox"]
+            for row in gemeinden_dict["features"]:
+                df_row = {"OBJID":row["properties"]["OBJID"], "BEZ_GEM":row["properties"]["BEZ_GEM"], "geometry":sg.shape(row["geometry"])}
+                cls.gemeinden_df = cls.gemeinden_df.append(df_row, ignore_index=True)
+        cls.qdt2 = index.PandasQuadtree(cls.gemeinden_df, "geometry", bbox=sg.box(*cls.gemeinden_bbox), capacity=10)
+                
+        # defining test query polygons for PandasQuadtree2
+        cls.query_polygon_df = pd.DataFrame(columns=["id", "overlap_id", "geometry"])
+        with open(cls.gemeinden_query) as query_json:
+            query_dict = json.load(query_json)
+            for row in query_dict["features"]:
+                df_row = {"id":row["properties"]["id"], "overlap_id":row["properties"]["overlap_id"], "geometry":sg.shape(row["geometry"])}
+                cls.query_polygon_df = cls.query_polygon_df.append(df_row, ignore_index=True)
+                
+     
+    def setUp(self):
         pass
         
+    def test_init(self):
+        pass
+         
+    def test_range_query(self):
         
-#===============================================================================
-# class QuadtreeHelper(unittest.TestCase):
-#     
-#     def setUp(self):
-#         xmin, ymin, xmax, ymax = 0.0, 0.0, 10.0, 10.0
-#         self.bbox = sg.box(xmin, ymin, xmax, ymax)
-#         
-#     def test_divide_quadtree(self):
-#         qdiv = index.QuadtreeHelper.divide_quadtree(self.bbox)
-#         print(qdiv.northwest.bounds)
-#         print(qdiv.northeast.bounds)
-#         print(qdiv.southeast.bounds)
-#         print(qdiv.southwest.bounds)
-#         
-# class QuadtreeDivision(unittest.TestCase):
-#     
-#     def setUp(self):
-#         self.nw = sg.box(0.0, 5.0, 5.0, 10.0)
-#         self.ne = sg.box(5.0, 5.0, 10.0, 10.0)
-#         self.se = sg.box(5.0, 0.0, 10.0, 5.0)
-#         self.sw = sg.box(0.0, 0.0, 5.0, 5.0)
-#         self.qd = index.QuadtreeDivision(self.nw, self.ne, self.se, self.sw)
-#         
-#     
-#     def test_get_item(self):
-#         for box in self.qd:
-#             print(box.bounds)  
-#===============================================================================
+        for i, row in self.query_polygon_df.iterrows():
+            geom = row["geometry"]
+            overlap_ids = row["overlap_id"]
+            overlap_ids = overlap_ids.split("|")
+            query_ids = [self.gemeinden_df.loc[i]["OBJID"] for i in self.qdt2.range_query(geom)]
+            overlap_ids.sort()
+            query_ids.sort()
+            self.assertEqual(overlap_ids, query_ids)
+        
+        
+class Test_Rectangle(unittest.TestCase):
+     
+    def setUp(self):
+        self.r1 = index.Rectangle(2.0, 2.0, 5.0, 4.0)
+        self.r2 = index.Rectangle(5.0, 4.0, 6.0, 6.0)
+        self.r3 = index.Rectangle(5.0, 1.0, 7.0, 3.0)
+        self.r4 = index.Rectangle(0.0, 1.0, 3.0, 2.0)
+        self.r5 = index.Rectangle(1.0, 1.0, 2.0, 7.0)
+         
+    def test_init(self):
+        pass
+    
+    def test_disjoint(self):
+        
+        # other touches on top right corner
+        self.assertFalse(self.r1.disjoint(self.r2))
+        # other touches on right edge
+        self.assertFalse(self.r1.disjoint(self.r3))
+        # other touches on bottom edge
+        self.assertFalse(self.r1.disjoint(self.r4))
+        # other touches on left edge
+        self.assertFalse(self.r1.disjoint(self.r5))
+        
+        # rectangles overlap
+        self.assertFalse(self.r4.disjoint(self.r5))
+        
+        
+class Test_RectangleDivision(unittest.TestCase):
+     
+    def setUp(self):
+        self.nw = index.Rectangle(0.0, 5.0, 5.0, 10.0)
+        self.ne = index.Rectangle(5.0, 5.0, 10.0, 10.0)
+        self.se = index.Rectangle(5.0, 0.0, 10.0, 5.0)
+        self.sw = index.Rectangle(0.0, 0.0, 5.0, 5.0)
+        self.qd = index.RectangleDivision(self.nw, self.ne, self.se, self.sw)
+        
+    def test_get_item(self):
+        self.assertEqual(self.qd[0], self.nw)
+        self.assertEqual(self.qd[1], self.ne)
+        self.assertEqual(self.qd[2], self.se)
+        self.assertEqual(self.qd[3], self.sw)
+        
         
 unittest.main()
